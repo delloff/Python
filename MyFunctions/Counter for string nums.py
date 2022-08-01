59676f678700111669d256e35fab9b544f1ceb5b
delloff <aal.daniloff@gmail.com>
2022-08-01 19:22:29 +0300

    Update Counter for string nums.py

--- MyFunctions/Counter for string nums.py
+++ MyFunctions/Counter for string nums.py
@@ -1,20 +1,19 @@
 def counter(s):
-    nl1 = ''
-    nl2 = ''
+    li0 = ''
+    li1 = ''
 
     for i in s:
         if s.startswith('0'):    
             if i == '0':
-                nl1 += i
+                li0 += i
             elif i != '0':
-                nl2 += i
-    else:
+                li1 += i
         return str(int(s) + 1).zfill(len(s))
     
     if all(c=='0' for c in s):
         x = 1
     else:
-        x = int(nl2) + 1
+        x = int(li1) + 1
     
     return str(x).zfill(len(s))
     
