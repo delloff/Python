7b85a74a2b86a55a162269d535668dd72bfad99d
delloff <aal.daniloff@gmail.com>
2022-08-01 15:53:24 +0300

    Create counter for string nums.py

---
+++ MyFunctions/counter for string nums.py
@@ -0,0 +1,22 @@
+def counter(s):
+    nl1 = ''
+    nl2 = ''
+
+    for i in s:
+        if s.startswith('0'):    
+            if i == '0':
+                nl1 += i
+            elif i != '0':
+                nl2 += i
+    else:
+        return str(int(s) + 1).zfill(len(s))
+    
+    if all(c=='0' for c in s):
+        x = 1
+        return str(x).zfill(len(s))
+    else:
+        x = int(nl2) + 1
+        return str(x).zfill(len(s))
+    
+
+print(counter('001'))   #002
