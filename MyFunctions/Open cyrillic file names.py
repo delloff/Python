b4da374b9aaedc4c0836ba2dc7c71052898548a0
delloff <aal.daniloff@gmail.com>
2022-08-03 11:27:18 +0300

    Create Open cyrillic file names.py

---
+++ MyFunctions/Open cyrillic file names.py
@@ -0,0 +1,13 @@
+import os
+import transliterate
+
+
+old_path = os.path.join("D:\Books", "Название на русском.xlsx")
+new_path = os.path.join("D:\Books", transliterate.translit('Название на русском.xlsx'.replace(' ','_'), reversed=True))
+
+os.rename(old_path, new_path)
+
+os.system(new_path)
+
+if not os.path.isfile(old_path):
+    os.rename(new_path, old_path)
