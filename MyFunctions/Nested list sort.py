2da01eac98ec8717b48e8527bca6371f307c80aa
delloff <aal.daniloff@gmail.com>
2022-08-03 11:25:07 +0300

    Update Nested list sort.py

--- MyFunctions/Nested list sort.py
+++ MyFunctions/Nested list sort.py
@@ -4,9 +4,9 @@ def nested_list_sort(arr):
     for i in range(len(arr)):
         for j in range(len(arr)-i-1):           # j == i because it accesses nested list, not nested list value
             if (arr[j][2] > arr[j + 1][2]):     # swap nested arrays here
-                tempo = arr[j]
+                temp = arr[j]
                 arr[j] = arr[j + 1]
-                arr[j + 1] = tempo
+                arr[j + 1] = temp
     return arr
 
 print(nested_list_sort([[3, 9, 16, 32], [7, 9, 85], [6, 4, 9]]))  #[[6, 4, 9], [3, 9, 16, 32], [7, 9, 85]]
