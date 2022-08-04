943548585d57e9916647a78790bf9bc18828e8c9
delloff <aal.daniloff@gmail.com>
2022-08-04 14:24:54 +0300

    Create Modify nested list in place.py

---
+++ MyFunctions/Modify nested list in place.py
@@ -0,0 +1,23 @@
+# replace 1 with 0 and vice versa
+
+arr =  [[1,1,0],
+        [1,0,1],
+        [0,0,0]]
+
+# option 1
+for row in A:
+    for index, val in enumerate(row):
+        row[index] = 0 if val > 0 else 1
+
+#-------------------------------------------
+
+# option 2
+for row in arr:
+    for index, val in enumerate(row):
+        if val > 0:
+            row[index] = 0
+        else:
+            row[index] = 1
+ 
+
+print(arr)
